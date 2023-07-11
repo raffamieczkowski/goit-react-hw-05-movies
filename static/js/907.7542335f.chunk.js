90900ff627169c1f647100cb21481654bc6b82b6
raffamieczkowski <108793865+raffamieczkowski@users.noreply.github.com>
2023-07-11 08:56:10 +0000

    Deploying to gh-pages from @ raffamieczkowski/goit-react-hw-05-movies@8bfdd7ab9736e2a6208db7491089156088b22aa8 🚀

--- static/js/907.7542335f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[907],{864:function(e,i,s){s.r(i);s(791);var r=s(87),n=s(689),c=s(184);i.default=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)("nav",{children:(0,c.jsxs)("ul",{children:[(0,c.jsx)("li",{children:(0,c.jsx)(r.rU,{to:"/",children:"Home"})}),(0,c.jsx)("li",{children:(0,c.jsx)(r.rU,{to:"/movies",children:"Movies"})})]})}),(0,c.jsx)("h1",{children:"Shared Layout"}),(0,c.jsx)(n.j3,{})]})}}}]);
-//# sourceMappingURL=907.7542335f.chunk.js.map
